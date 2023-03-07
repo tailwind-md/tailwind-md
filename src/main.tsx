f832569d24efe769bda5d218a1401b06b25a93d3
Clarence Manuel <rencedm112@gmail.com>
2023-03-07 22:46:46 +0800

    Update

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import { render } from "solid-js/web";
 import "./app.postcss";
 
 const routes = import.meta.glob("./routes/**/*.{tsx,ts}", { eager: true });
-
 console.log(routes);
 
 render(() => <Showcase />, document.getElementById("app"));
